6c344d2a655dd4597a0e908b2c84075d65f95785
swapnil.davangave <swapnil.davangave@zero.ai>
2018-06-11 04:36:56 +0530

    added test.py

---
+++ tests/test.py
@@ -0,0 +1,33 @@
+from sqlalchemy 				import Column, DateTime, String, Integer, ForeignKey, func
+from sqlalchemy.orm 			import relationship, backref
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm 			import validates
+from sqlalchemy 				import create_engine
+from sqlalchemy.orm 			import sessionmaker
+import os.path
+import json
+import unittest
+
+
+class TestContact(unittest.TestCase):
+    
+    def setUp(self):
+        pass
+
+class TestGroup(unittest.TestCase):
+    
+    def setUp(self):
+        pass
+
+class TestContactGroup(unittest.TestCase):
+    
+    def setUp(self):
+        pass
+
+class TestCSVToPostgres(unittest.TestCase)
+    
+    def setUp(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
